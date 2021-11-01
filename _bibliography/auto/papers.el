435d7161a389f0e75069f89b07b87c4ded73c04e
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-11-01 15:24:15 +0000

    Merge branch 'alshedivat-master' [ci skip]

--- _bibliography/auto/papers.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "papers"
- (lambda ()
-   (LaTeX-add-bibitems
-    "DBLP:conf/rc/FabbrettiLS21"))
- :bibtex)
-
--- _news/announcement_1.md
+++
@@ -1,7 +0,0 @@
----
-layout: post
-date: 20-07-2021 15:59:00-0400
-inline: true
----
-
-I have finally created my website. :-)
